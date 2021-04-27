f7847546ae1aa13a046f4cc551abd3b6c77fa5a9
lu_weicong <lu_weicong@purvar.co.jp>
2021-04-27 17:27:05 +0900

    no message

--- src/main/java/com/example/demo/httpclient/GetExample.java
+++ src/main/java/com/example/demo/httpclient/GetExample.java
@@ -14,15 +14,17 @@ public class GetExample {
 	public static void main(String[] args) {
 		OkHttpClient client = new OkHttpClient();
 
-//		String url = "http://127.0.0.1:8080/springbootdemo/employee/select/1";
-		String url = "http://127.0.0.1:8080/springbootdemo/employee/insertTestData";
+		String url = "http://127.0.0.1:8080/springbootdemo/employee/select/111";
+//		String url = "http://127.0.0.1:8080/springbootdemo/employee/insertTestData";
 		String result = null;
 
 		Request request = new Request.Builder().url(url).build(); // <-- 请求
 
 		// 注意这里try的写法，运行完后会自动关闭Response
 		try (Response response = client.newCall(request).execute()) { // <-- 返回
-			result = response.body().string(); // <-- 返回的ResponseBody
+			if (response.isSuccessful()) { // 如果请求成功
+				result = response.body().string(); // <-- 返回的ResponseBody
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
